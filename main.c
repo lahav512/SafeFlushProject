e6365cceda8109d3672697f450ffaab7c09223a3
Lahav <lahavs512@gmail.com>
2021-07-24 15:04:20 +0300

    Examples have been modified.

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 #include "SafeFlushAPI/Headers/safe_flush.h"
 
 int main() {
@@ -11,9 +10,16 @@ int main() {
     char * chunk = "0101110101010111111111111111010100000111000110101100010101";
     long address = BLOCK_BITS - 4;
 
+    // Read and print a the section of the storage.
     flushAPI.read(address, (long) strlen(chunk), &data);
     printf("%s\n", data);
 
+    // Safely write 'chunk' into the same section of the storage.
     safeFlushAPI.safeWrite(address, (long) strlen(chunk), chunk);
+
+    // Read and print a the new section of the storage which should be identical to 'chunk'.
+    flushAPI.read(address, (long) strlen(data), &data);
+    printf("%s\n", data);
+
     return 0;
 }
